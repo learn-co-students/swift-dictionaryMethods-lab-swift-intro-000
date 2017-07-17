8c898ed3aca194b8603072f162839eda69c05557
Mitchell Schuster <mgschuster11@gmail.com>
2017-07-17 21:04:46 +0300

    Finished

--- DictionaryMethods/DictionaryMethods.swift
+++ DictionaryMethods/DictionaryMethods.swift
@@ -12,36 +12,91 @@ class DictionaryMethods {
     
     // Questions #1, #2, #3, #6 and #7
     
-
+    var starWarsHeroes: [String] = ["Luke Skywalker", "Princess Leia", "Han Solo", "Rey"]
+    var starWarsVillains: [String] = ["Darth Vader", "Emperor Palpatine"]
+    var starWarsDroids: [String] = ["R2-D2", "C-3P0", "IG-88", "BB-8"]
+    var starWarsGangsters: [String] = ["Watto", "Jabba the Hutt"]
+    var starWarsCharacters = [String:[String]]()
+    
     
     // Question #4
    
-    
+    func addKyloRen() {
+        starWarsVillains.append("Kylo Ren")
+    }
     
     
     // Question #5
   
+    func remove(droid: String) -> Bool {
+        
+        var index = 0
+        var boolReturn = true
+        
+        
+        for droidName in starWarsDroids {
+            
+            if droid == droidName {
+                starWarsDroids.remove(at: index)
+                boolReturn = true
+                return boolReturn
+            } else {
+                boolReturn = false
+                index += 1
+                return boolReturn
+            }
+            
+        }
+
+        return boolReturn
+
+    }
+
     
     
     
     // Question #6
    
-    
+    func createStarWarsCharacters() {
+        starWarsCharacters.updateValue(starWarsHeroes, forKey: "Heroes")
+        starWarsCharacters.updateValue(starWarsVillains, forKey: "Villains")
+        starWarsCharacters.updateValue(starWarsDroids, forKey: "Droids")
+    }
     
     
     // Question #7
    
-    
+    func createStarWarsGangsters() {
+        starWarsCharacters.updateValue(starWarsGangsters, forKey: "Gangsters")
+    }
     
     
     // Question #8
     
-    
+    func description(characters: [String:[String]]) -> String {
+        var sentence = "--Star Wars Chararcters--"
+        
+        for (type, names) in characters {
+            sentence += "\n\(type.uppercased())\n"
+            
+            for (index, names) in names.enumerated() {
+                sentence += "\n\(index+1). \(names)\n"
+            }
+            
+        }
+        
+        return sentence
+        
+    }
     
     
     // Question #9
     
-    
+    func addHearts() {
+        for (_, names) in starWarsCharacters {
+            
+        }
+    }
     
     
 }
