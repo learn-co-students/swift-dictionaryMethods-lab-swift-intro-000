45da93147478a67a639b23c3be242c0115fa1ead
asyzdykov <internetcafe@bk.ru>
2017-11-24 01:26:28 +0600

    tests completed

--- DictionaryMethods/DictionaryMethods.swift
+++ DictionaryMethods/DictionaryMethods.swift
@@ -12,36 +12,62 @@ class DictionaryMethods {
     
     // Questions #1, #2, #3, #6 and #7
     
-
+    var starWarsHeroes : [String] = ["Luke Skywalker","Princess Leia","Han Solo","Rey"]
+    var starWarsVillains : [String] = ["Darth Vader", "Emperor Palpatine"]
+    var starWarsDroids : [String] = ["R2-D2","C-3P0","IG-88","BB-8"]
+    var starWarsGangsters : [String] = ["Watto","Jabba the Hutt"]
+    var starWarsCharacters = [String:[String]]()
+    
+    
+    
     
     // Question #4
-   
     
+    func addKyloRen()  {
+        starWarsVillains.append("Kylo Ren")
+    }
     
     
     // Question #5
-  
+    func remove(droid:String) -> Bool {
+        if let one = starWarsDroids.index(of: droid) {
+            starWarsDroids.remove(at: one)
+            return true
+        }
+        return false
+    }
     
     
     
     // Question #6
-   
-    
-    
-    
+    func createStarWarsCharacters()  {
+        starWarsCharacters ["Heroes"] = starWarsHeroes
+        starWarsCharacters ["Villains"] = starWarsVillains
+        starWarsCharacters ["Droids"] = starWarsDroids
+    }
     // Question #7
-   
-    
-    
-    
+    func createStarWarsGangsters() {
+        starWarsCharacters ["Gangsters"] = starWarsGangsters
+    }
     // Question #8
-    
-    
-    
-    
+    func description(characters:[String:[String]]) -> String {
+        var sentence = "--Star Wars Characters--"
+        for (type, names) in characters {
+            sentence += "\n\(type.uppercased())\n"
+            for (num, name) in names.enumerated() {
+                sentence += "\(num+1). \(name)\n"
+            }
+        }
+        return sentence
+    }
     // Question #9
-    
-    
+    func addHearts() {
+        for (type, names) in starWarsCharacters {
+            for (idx,name) in names.enumerated(){
+             starWarsCharacters[type]![idx] = name.replacingOccurrences(of: "o", with: "❤️")
+            }
+        }
+    }
     
     
 }
