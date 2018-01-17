2eaeddbf7c298b3bc8ee458e294095ab88b75d8e
Jaldhar H. Vyas <jaldhar@braincells.com>
2018-01-16 23:42:26 -0500

    Completed.

--- DictionaryMethods/DictionaryMethods.swift
+++ DictionaryMethods/DictionaryMethods.swift
@@ -10,38 +10,81 @@ import Foundation
 
 class DictionaryMethods {
     
-    // Questions #1, #2, #3, #6 and #7
-    
+    var starWarsHeroes: [String] = [
+        "Luke Skywalker",
+        "Princess Leia",
+        "Han Solo",
+        "Rey",
+    ]
 
+    var starWarsVillains: [String] = [
+        "Darth Vader",
+        "Emperor Palpatine"
+    ]
+
+    var starWarsDroids: [String] = [
+        "R2-D2",
+        "C-3P0",
+        "IG-88",
+        "BB-8"
+    ]
+
+    var starWarsGangsters: [String] = [
+        "Watto",
+        "Jabba The Hutt"
+    ]
+    var starWarsCharacters: [String: [String]] = [:]
+
+    func addKyloRen() -> Void {
+        starWarsVillains.append("Kylo Ren")
+    }
     
-    // Question #4
-   
+    func remove(droid: String) -> Bool {
+
+        for i: Int in starWarsDroids.startIndex ..< starWarsDroids.endIndex {
+            if starWarsDroids[i] == droid {
+                starWarsDroids.remove(at: i)
+                return true
+            }
+        }
+        return false
+    }
+    
+    func createStarWarsCharacters() -> Void {
+        starWarsCharacters["Heroes"] = starWarsHeroes
+        starWarsCharacters["Villains"] = starWarsVillains
+        starWarsCharacters["Droids"] = starWarsDroids
+    }
+    
+    func createStarWarsGangsters() -> Void {
+        starWarsCharacters["Gangsters"] = starWarsGangsters
+    }
     
     
     
-    // Question #5
-  
-    
-    
-    
-    // Question #6
-   
-    
-    
-    
-    // Question #7
-   
-    
-    
-    
-    // Question #8
+    func description(characters: [String: [String]]) -> String {
+        var sentence: String = "--Star Wars Characters--\n"
+
+        for (type, names) in characters {
+            sentence.append("\(type.uppercased())\n")
+            for (offset, element) in names.enumerated() {
+                sentence.append("\(offset + 1). \(element)\n")
+            }
+        }
+
+        return sentence
+    }
     
     
     
     
-    // Question #9
-    
-    
-    
-    
+    func addHearts() -> Void {
+        for (type, names) in starWarsCharacters {
+            var newNames: [String] = []
+            for name in names {
+                newNames.append(name.replacingOccurrences(of: "o", with: "♥️"))
+            }
+            starWarsCharacters[type] = newNames
+        }
+    }
 }
