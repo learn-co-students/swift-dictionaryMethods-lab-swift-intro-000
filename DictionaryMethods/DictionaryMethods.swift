0982e9980327cbe5ddb2e08814a8469bb4548a1e
giddix23 <giddixcompany@gmail.com>
2018-06-19 22:06:55 -0400

    dictMethods

--- DictionaryMethods/DictionaryMethods.swift
+++ DictionaryMethods/DictionaryMethods.swift
@@ -10,38 +10,87 @@ import Foundation
 
 class DictionaryMethods {
     
-    // Questions #1, #2, #3, #6 and #7
+    var starWarsHeroes = ["Luke Skywalker", "Princess Leia", "Han Solo", "Rey"]
+    var starWarsVillains = ["Darth Vader", "Emperor Palpatine"]
+    var starWarsDroids = ["R2-D2", "C-3P0", "IG-88", "BB-8"]
+    var starWarsGangsters = ["Watto", "Jabba the Hutt"]
+    var starWarsCharacters: [String:[String]] = [:]
     
 
-    
-    // Question #4
+    func addKyloRen(){
+        starWarsVillains.append("Kylo Ren")
+    }
    
     
     
-    
-    // Question #5
+    func remove(droid: String) -> Bool {
+        var indexCount = 0
+        for i in starWarsDroids {
+            if i == droid {
+                starWarsDroids.remove(at: indexCount )
+                return true
+            } else {
+                indexCount += 1
+            }
+        }
+        return false
+    }
+        
   
     
     
     
-    // Question #6
+    func createStarWarsCharacters(){
+        starWarsCharacters["Heroes"] = starWarsHeroes
+        starWarsCharacters["Villains"] = starWarsVillains
+        starWarsCharacters["Droids"] = starWarsDroids
+            
+        }
    
     
     
-    
-    // Question #7
-   
-    
-    
-    
-    // Question #8
+    func createStarWarsGangsters(){
+        starWarsCharacters["Gangsters"] = starWarsGangsters
+            
+        }
     
     
+    func description(characters: [String: [String]]) -> String {
+        
+        var sentence = "--Star Wars Characters--"
+        
+        for (type, names) in characters{
+            sentence += "\n\(type.uppercased())\n"
+            for (index, name) in names.enumerated() {
+                sentence += "\(index + 1). \(name)"
+            }
+        }
+        return sentence
+    }
     
     
-    // Question #9
     
     
+    func addHearts() {
+        for (type, var names) in starWarsCharacters {
+            var i = 0
+            for var name in names {
+                if name.contains("o"){
+                   name = name.replacingOccurrences(of: "o", with: "❤️")
+                   names[i] = name
+                    print(names[i])
+                    i += 1
+                } else {
+                    print(names[i])
+                    i += 1
+                }
+            }
+        }
+    }
     
-    
+      
 }
+    
+    
+    
+
